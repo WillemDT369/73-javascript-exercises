43fdd1c1e722dc24dc465d87068e59e2965b908f
WillemDT369 <detroyerwillem@hotmail.com>
2020-11-09 11:20:05 +0100

    add var-prompt

--- 01-base/02-var-prompt/script.js
+++ 01-base/02-var-prompt/script.js
@@ -9,8 +9,9 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function(myfunction) {
 
-    // your code here
-
+    var name = prompt("please enter your name", "enter your name here");
+    var greeting = "Hello "
+    alert(greeting + name);
 })();

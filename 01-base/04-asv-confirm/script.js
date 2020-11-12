b3e465e3f44a9150d1aff94644100ead22b43ab7
WillemDT369 <detroyerwillem@hotmail.com>
2020-11-12 11:22:47 +0100

    update exercise with new things to try

--- 01-base/04-asv-confirm/script.js
+++ 01-base/04-asv-confirm/script.js
@@ -23,3 +23,33 @@
             generalinfo()
         }
 })();
+
+
+//trying this one to work, later
+
+// (function generalinfo() {
+//     var age = prompt("How old are you?");
+//     var result = confirm("C'mon bruh gonna tell me?");
+//         while (result == false){
+//             result = confirm("C'mon bruh how old are you?");
+//         }
+//         if (result == true){
+//             age = prompt("Thanks for changing your mind.")
+//         }
+//     var gender = prompt("What is your gender?");
+//         if (gender == false){
+//             prompt("What is your gender?");
+//         }
+//     var location = prompt("Where do you live?");
+//         if (location == false){
+//             prompt("Where do you live?");
+//         }
+//     var info = confirm(age + " " + gender + " " + location);
+//         if (info == true){
+//             alert("nice, you did it!")
+//             // return true
+//         }
+//         else{
+//             generalinfo()
+//         }
+// })();

21126d68be326ffde3e09d359770f3b0162616d1
WillemDT369 <detroyerwillem@hotmail.com>
2020-11-26 11:28:42 +0100

    Display new text

---
+++ 08-misc/02-typewriter-effect/script.js
@@ -0,0 +1,23 @@
+/* becode/javascript
+ *
+ * /07-misc/02-typewriter-effect/script.js - 7.2: effet machine à écrire
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // your code here
+
+    // display new text
+    let newText = document.getElementById("target");
+    newText.innerHTML = "De knappe kapper knipt knap, maar de knecht van de knappe kapper knipt knapper dan de knappe kapper knipt.";
+    
+
+    
+
+})();

5bdf25ec082e6c64a5c868c142cff1a9c4f1c261
WillemDT369 <detroyerwillem@hotmail.com>
2020-11-28 17:52:36 +0100

    Select buttons

---
+++ 08-misc/04-worst-ui-two/script.js
@@ -0,0 +1,53 @@
+/* becode/javascript
+ *
+ * /07-misc/04-worst-ui-two/script.js - 7.4: la pire interface (2) : phone clicker
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function () {
+    let output = document.getElementById("target");
+
+    // select button and listen to a click to do something
+    document.getElementById("part-one").addEventListener("click", function(){
+        console.log("working now?");
+    });
+    document.getElementById("part-two").addEventListener("click", function(){
+        console.log("working now too?");
+    });
+    document.getElementById("part-three").addEventListener("click", function(){
+        console.log("working now three?");
+    });
+    document.getElementById("part-four").addEventListener("click", function(){
+        console.log("working now four?");
+    });
+
+
+
+
+
+
+
+
+
+
+    // let btn1 = document.getElementById("part-one");
+    // let btn2 = document.getElementById("part-two");
+    // let btn3 = document.getElementById("part-three");
+    // let btn4 = document.getElementById("part-four");
+    // btn1.addEventListener("click", clicker);
+    
+    // function clicker(){
+    //     console.log("heyhey");
+    // }
+    
+
+
+
+
+
+})();
